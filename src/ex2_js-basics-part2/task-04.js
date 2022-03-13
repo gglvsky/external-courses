a3f0cec60fb58badbb80bf94bba5c5ade28e64fc
gglvsky <49413171+gglvsky@users.noreply.github.com>
2022-03-13 16:11:53 +0300

    Update task-04.js

--- src/ex2_js-basics-part2/task-04.js
+++ src/ex2_js-basics-part2/task-04.js
@@ -3,8 +3,9 @@ function arrayElementsComparison(arr) {
   for (let i = 0; i < arr.length; i += 1) {
     if (arr[0] === arr[i]) {
       return true;
-    }
+    } else {
     return false;
+    }
   }
 }
 module.exports = arrayElementsComparison;
